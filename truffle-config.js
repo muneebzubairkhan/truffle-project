b150ca439385e374453abf06db8ae238de75022a
timon#1213 <money.bird.mi@gmail.com>
2021-05-23 06:22:24 +0500

    deployed https://testnet.bscscan.com/address/0x10e0BC6EBCf6B07D2c8457C806AbCf8Fa0bDA2b7#contracts

--- truffle-config.js
+++ truffle-config.js
@@ -1,92 +1,85 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-// const infuraKey = "fj4jll3k.....";
-//
-const fs = require("fs");
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+var HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+const MNEMONIC = process.env.MNEMONIC;
+const token = process.env.INFURA_TOKEN;
+const etherscanKey = process.env.BSCSCAN_KEY;
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
+    develop: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
+      gas: 6721975,
+    },
     development: {
-      host: "127.0.0.1", // Localhost (default: none)
-      port: 8545, // Standard Ethereum port (default: none)
-      network_id: "*", // Any network (default: none)
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*',
+      gas: 6721975,
     },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
+    bscTestnet: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          'https://data-seed-prebsc-1-s1.binance.org:8545'
+        );
+      },
+      network_id: '97',
+    },
+
+    bscMainnet: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          'https://bsc-dataseed.binance.org'
+        );
+      },
+      network_id: '56',
+    },
+
+    mainnet: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          'https://mainnet.infura.io/v3/' + token
+        );
+      },
+      network_id: '1',
+    },
+    ropsten: {
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          'https://ropsten.infura.io/v3/' + token
+        );
+      },
+      network_id: '3',
+    },
     kovan: {
-      provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          `https://kovan.infura.io/v3/a30ce8978acb4d7da82e6d7e6b71afb7`
-        ),
-      network_id: 42, // Ropsten's id
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+      provider: () => {
+        return new HDWalletProvider(
+          MNEMONIC,
+          'https://kovan.infura.io/v3/' + token
+        );
+      },
+      network_id: '42',
+      skipDryRun: true,
     },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
   },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
+  plugins: ['truffle-plugin-verify'],
+  api_keys: {
+    etherscan: etherscanKey,
   },
-
-  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.0", // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      version: '0.8.0',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
     },
-  },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
-  //
-  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
-  // those previously migrated contracts available in the .db directory, you will need to run the following:
-  // $ truffle migrate --reset --compile-all
-
-  db: {
-    enabled: false,
-  },
-
-  plugins: ["truffle-plugin-verify"],
-  api_keys: {
-    etherscan: "HSR39CEBP479H5TBJTXR693XMATW8T68KE",
   },
 };
