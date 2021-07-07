89e1ed8b12e1e8e14984fb842c8775b98de9d424
Muneeb <thinkmuneeb@gmail.com>
2021-07-07 13:24:29 +0500

    test successfull

--- contracts/Presale.sol
+++ contracts/Presale.sol
@@ -3,27 +3,24 @@ pragma solidity 0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// functions: setRate, buyTokens
+// functions: setRate, buyTokens, unlockable...
 contract Presale is Ownable {
-    IERC20 token;
-    IERC20 usdt;
-    uint256 tokensSold = 0;
-    uint256 rate = 3000; // 0.3 = 3000, 0.45 = 4500, 0.69 = 6900 // 0.3 USDT = 1 ARI, rate = 0.3, 0.45, 0.69
-    address wallet;
+    IERC20 public token;
+    IERC20 public usdt;
+    uint256 public tokensSold = 0;
+    uint256 public rate = 3000; // 0.3 = 3000, 0.45 = 4500, 0.69 = 6900 // 0.3 USDT = 1 ARI
+    address public wallet;
 
     constructor(
         IERC20 _token,
         IERC20 _usdt,
         address _wallet
     ) {
+        token = _token;
         usdt = _usdt;
         wallet = _wallet;
-        token = _token;
     }
 
-    // do the natspec annotations
-    // input: buyTokens(100), expected output: person charged 100*0.3 = $30 and gets 100 tokens
-    // 1 test case will be formed
     function buyTokens(uint256 _tokens) public {
         tokensSold += _tokens;
         usdt.transferFrom(msg.sender, wallet, (_tokens * rate) / 10000);
@@ -33,9 +30,8 @@ contract Presale is Ownable {
     function setRate(uint256 _rate) public onlyOwner {
         rate = _rate;
     }
-}
 
-/*
-10 10*1
-10 10*0.3 = 3
-*/
+    function getTokens(IERC20 _token, uint256 _amount) public onlyOwner {
+        _token.transfer(owner(), _amount);
+    }
+}
---
+++ test/Presale.test.js
@@ -0,0 +1,49 @@
+const USDT = artifacts.require("USDT");
+const ARI = artifacts.require("ARI");
+const Presale = artifacts.require("Presale");
+
+contract("Presale", async ([tokenOwner, wallet, client, usdtOwner]) => {
+  // make it like real test case, input: buyTokens(100), expected output: person charged 100*0.3 = $30 and gets 100 tokens
+  it("input: buyTokens(100), expected output: person charged 100*0.3 = $30 and gets 100 tokens.", async () => {
+    // Variables Init {
+    const token = await ARI.new({ from: tokenOwner });
+    const usdt = await USDT.new({ from: usdtOwner });
+
+    // this line will not happen in real life
+    await usdt.transfer(client, web3.utils.toWei("100"), { from: usdtOwner }); // give 100$ to client
+
+    const presale = await Presale.new(token.address, usdt.address, wallet, {
+      from: tokenOwner,
+    });
+
+    const tokensForPresale = web3.utils.toWei("250000000");
+    await token.transfer(presale.address, tokensForPresale, {
+      from: tokenOwner,
+    });
+
+    // }
+    console.log(
+      "BEFORE (await token.balanceOf(client)).toNumber()",
+      web3.utils.fromWei((await token.balanceOf(client)).toString())
+    );
+    console.log(
+      "(await usdt.balanceOf(client)).toNumber()",
+      web3.utils.fromWei((await usdt.balanceOf(client)).toString())
+    );
+
+    await usdt.approve(presale.address, MAX_INT, { from: client });
+    await presale.buyTokens(web3.utils.toWei("100"), { from: client });
+
+    console.log(
+      "AFTER (await token.balanceOf(client)).toNumber()",
+      web3.utils.fromWei((await token.balanceOf(client)).toString())
+    );
+    console.log(
+      "(await usdt.balanceOf(client)).toNumber()",
+      web3.utils.fromWei((await usdt.balanceOf(client)).toString())
+    );
+  });
+});
+
+const MAX_INT =
+  "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
