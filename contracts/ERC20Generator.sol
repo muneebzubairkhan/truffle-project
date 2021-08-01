11e8b91079196b2f4fe9da1e75782601fe714db5
Muneeb <thinkmuneeb@gmail.com>
2021-08-01 12:53:03 +0500

    deployed to BSC Testnet

--- contracts/ERC20Generator.sol
+++ contracts/ERC20Generator.sol
@@ -3,15 +3,18 @@ pragma solidity ^0.8.0;
 import "./TokenERC20.sol";
 
 contract ERC20Generator {
-    uint256 _1000_coins = 1000 * 1e18;
-    IERC20[] erc20s;
+    IERC20[] public erc20s;
 
     function createERC20(
         string memory _name,
         string memory _symbol,
         uint256 _totalSupply
-    ) public {
+    ) external {
         IERC20 erc20 = new TokenERC20(_name, _symbol, _totalSupply);
         erc20s.push(erc20);
+    }
+
+    function getERC20s() external view returns (IERC20[] memory) {
+        return erc20s;
     }
 }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,16 +2,18 @@ const Migrations = artifacts.require("Migrations");
 const USDT = artifacts.require("USDT");
 const RedToken = artifacts.require("RedToken");
 const BlueToken = artifacts.require("BlueToken");
+const ERC20Generator = artifacts.require("ERC20Generator");
 
 module.exports = async (deployer, network, accounts) => {
-  await localDeploy(deployer, accounts);
+  await defaultDeploy(deployer, accounts);
   // await rinkeby(deployer, accounts);
   // await ethMainnet(deployer);
 };
 
-const localDeploy = async (deployer, [_, client, owner, dev]) => {
+const defaultDeploy = async (deployer, [_, client, owner, dev]) => {
   await deployer.deploy(Migrations);
 
+  const erc20Generator = await deployer.deploy(ERC20Generator);
   const usdt = await deployer.deploy(USDT);
   const redToken = await deployer.deploy(RedToken);
   const blueToken = await deployer.deploy(BlueToken);
@@ -19,17 +21,10 @@ const localDeploy = async (deployer, [_, client, owner, dev]) => {
   console.log(`const usdt = "${usdt.address}";`);
   console.log(`const redToken = "${redToken.address}";`);
   console.log(`const blueToken = "${blueToken.address}";`);
+  console.log(`const erc20Generator = "${erc20Generator.address}";`); 
   console.log(`// const migrations = "${Migrations.address}";`);
 };
 
-const rinkeby = async (deployer, accounts) => {
-  await deployer.deploy(Migrations);
-
-  const usdt = await USDT.at();
-  const red = await RedToken.at();
-  const blue = await BlueToken.at();
-
-  console.log("usdt", usdt.address);
-};
+const rinkeby = async (deployer, accounts) => {};
 
 const ethMainnet = async (deployer) => {};
