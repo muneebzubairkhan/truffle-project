c814bfb8049c9aa75f09e336a784b74515b31e37
Muneeb <thinkmuneeb@gmail.com>
2021-08-10 09:17:56 +0500

    deployed TokenERC20 https://rinkeby.etherscan.io/address/0xEf44f26371BF874b5D4c8b49914af169336bc957#contracts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,6 +3,7 @@ const USDT = artifacts.require("USDT");
 const RedToken = artifacts.require("RedToken");
 const BlueToken = artifacts.require("BlueToken");
 const ERC20Generator = artifacts.require("ERC20Generator");
+const TokenERC20 = artifacts.require("TokenERC20");
 
 module.exports = async (deployer, network, accounts) => {
   await defaultDeploy(deployer, accounts);
@@ -13,7 +14,14 @@ module.exports = async (deployer, network, accounts) => {
 const defaultDeploy = async (deployer, [_, client, owner, dev]) => {
   await deployer.deploy(Migrations);
 
-  const erc20Generator = await deployer.deploy(ERC20Generator);
+  const erc20Token = await deployer.deploy(
+    TokenERC20,
+    _,
+    "USDC",
+    "USDC",
+    toWei("1000")
+  );
+  // const erc20Generator = await deployer.deploy(ERC20Generator);
   // const usdt = await deployer.deploy(USDT);
   // const redToken = await deployer.deploy(RedToken);
   // const blueToken = await deployer.deploy(BlueToken);
@@ -21,10 +29,13 @@ const defaultDeploy = async (deployer, [_, client, owner, dev]) => {
   // console.log(`const usdt = "${usdt.address}";`);
   // console.log(`const redToken = "${redToken.address}";`);
   // console.log(`const blueToken = "${blueToken.address}";`);
-  console.log(`const erc20Generator = "${erc20Generator.address}";`); 
+  console.log(`const erc20Token = "${erc20Token.address}";`);
+  // console.log(`const erc20Generator = "${erc20Generator.address}";`);
   console.log(`// const migrations = "${Migrations.address}";`);
 };
 
 const rinkeby = async (deployer, accounts) => {};
 
 const ethMainnet = async (deployer) => {};
+
+const toWei = web3.utils.toWei;
