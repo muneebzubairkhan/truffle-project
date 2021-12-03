7cbf9ebea0cbce23fc11c3caa20c6da98e3f1435
Muneeb <thinkmuneeb@gmail.com>
2021-12-03 20:49:02 +0500

    ok

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
 const Migrations = artifacts.require('Migrations');
-const MarketPlace = artifacts.require('Marketplace-b')
+const MarketPlace = artifacts.require('NFTMarket');
 const Biding = artifacts.require('Biding');
 const networksConfig = require('../truffle-config');
 const { makeHelperCodeForUIDev } = require('./helper');
@@ -10,9 +10,6 @@ module.exports = async (deployer, network, accounts) => {
   // console.log('network: ', network);
   await deployer.deploy(Migrations);
   const biding = await deployer.deploy(Biding);
-  await deployer.deploy(
-    MarketPlace,
-    '0xc18E78C0F67A09ee43007579018b2Db091116B4C',
-  );
-  // makeHelperCodeForUIDev({ biding, marketplace });
+  const marketplace = await deployer.deploy(MarketPlace);
+  makeHelperCodeForUIDev(network,{ biding, marketplace });
 };
