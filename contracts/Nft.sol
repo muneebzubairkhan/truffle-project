f4c71ff7b2e42df79db7b44ced4faa4935bb6dbd
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-06 22:34:23 +0500

    init

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -1,52 +1,38 @@
-// SPDX-License-Identifier: GPL-3.0
+// Hi. If you have any questions or comments in this smart contract please let me know at:
+// Whatsapp +923014440289, Telegram @thinkmuneeb, discord: timon#1213, I'm Muneeb Zubair Khan
+//
+//
+// Smart Contract Made by Muneeb Zubair Khan
+// The UI is made by Abraham Peter, Whatsapp +923004702553, Telegram @Abrahampeterhash.
+//
+//
+//
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.9;
 
-pragma solidity >=0.7.0 <0.9.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract NFT is ERC721Enumerable, Ownable {
-  using Strings for uint256;
-
-  string public baseURI;
-  string public baseExtension = ".json";
-  string public notRevealedUri;
-  uint256 public cost = 1 ether;
-  uint256 public maxSupply = 10000;
-  uint256 public maxMintAmount = 20;
-  uint256 public nftPerAddressLimit = 3;
-  bool public paused = false;
-  bool public revealed = false;
-  bool public onlyWhitelisted = true;
-  mapping(address => uint256) public addressMintedBalance;
-
-    // white list variables
+contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
+    string public baseURI;
+    bool public isSaleActive;
+    uint256 public circulatingSupply;
+    address public owner = msg.sender;
+    uint256 public itemPrice = 0.08 ether;
     uint256 public itemPricePresale = 0.06 ether;
+    uint256 public constant totalSupply = 10_333;
+
+    address public marketing = 0x15b5C64EA09dC8cdc1ad31869d2Aa64f5Dd80377;
+    address public dev = 0xc66C9f79AAa0c8E6F3d12C4eFc7D7FE7c1f8B89C;
+
     bool public isAllowListActive;
     uint256 public allowListMaxMint = 3;
     mapping(address => bool) public onAllowList;
     mapping(address => uint256) public allowListClaimedBy;
 
-  constructor(
-    string memory _name,
-    string memory _symbol,
-    string memory _initBaseURI,
-    string memory _initNotRevealedUri
-  ) ERC721(_name, _symbol) {
-    setBaseURI(_initBaseURI);
-    setNotRevealedURI(_initNotRevealedUri);
-  }
-
-  // internal
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
-  }
-
     ////////////////////
     //   ALLOWLIST    //
     ////////////////////
-
     function addToAllowList(address[] calldata addresses) external onlyOwner {
         for (uint256 i = 0; i < addresses.length; i++)
             onAllowList[addresses[i]] = true;
@@ -60,114 +46,87 @@ contract NFT is ERC721Enumerable, Ownable {
             onAllowList[addresses[i]] = false;
     }
 
-    function purchasePresaleTokens(uint256 _mintAmount)
+    ////////////////////
+    //    PRESALE     //
+    ////////////////////
+
+    // Purchase multiple NFTs at once
+    function purchasePresaleTokens(uint256 _howMany)
         external
         payable
+        tokensAvailable(_howMany)
     {
-         require(_mintAmount > 0, "need to mint at least 1 NFT");
-        uint256 supply = totalSupply();
-        require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
         require(isAllowListActive, "Allowlist is not active");
         require(onAllowList[msg.sender], "You are not in allowlist");
         require(
-            allowListClaimedBy[msg.sender] + _mintAmount <= allowListMaxMint,
+            allowListClaimedBy[msg.sender] + _howMany <= allowListMaxMint,
             "Purchase exceeds max allowed"
         );
         require(
-            msg.value >= _mintAmount * itemPricePresale,
+            msg.value >= _howMany * itemPricePresale,
             "Try to send more ETH"
         );
 
-        allowListClaimedBy[msg.sender] += _mintAmount;
+        allowListClaimedBy[msg.sender] += _howMany;
 
-        for (uint256 i = 1; i <= _mintAmount; i++)
-            _safeMint(msg.sender, supply + i);
-    
+        for (uint256 i = 0; i < _howMany; i++)
+            _mint(msg.sender, ++circulatingSupply);
     }
 
-  // public
-  function mint(uint256 _mintAmount) public payable {
-    require(!paused, "the contract is paused");
-    uint256 supply = totalSupply();
-    require(_mintAmount > 0, "need to mint at least 1 NFT");
-    require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
-    require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
-    require(msg.value >= cost * _mintAmount, "insufficient funds");
-    
-    for (uint256 i = 1; i <= _mintAmount; i++)
-       _safeMint(msg.sender, supply + i);
+    ////////////////////
+    //  PUBLIC SALE   //
+    ////////////////////
 
-  }
-  
-  
+    // Purchase multiple NFTs at once
+    function purchaseTokens(uint256 _howMany)
+        external
+        payable
+        tokensAvailable(_howMany)
+    {
+        require(
+            isSaleActive,
+            "Sale is not active"
+        );
+        require(_howMany > 0 && _howMany <= 10, "Mint min 1, max 10");
+        require(msg.value >= _howMany * itemPrice, "Try to send more ETH");
 
-  function walletOfOwner(address _owner)
-    public
-    view
-    returns (uint256[] memory)
-  {
-    uint256 ownerTokenCount = balanceOf(_owner);
-    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-    for (uint256 i; i < ownerTokenCount; i++) {
-      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
-    }
-    return tokenIds;
-  }
-
-  function tokenURI(uint256 tokenId)
-    public
-    view
-    virtual
-    override
-    returns (string memory)
-  {
-    require(
-      _exists(tokenId),
-      "ERC721Metadata: URI query for nonexistent token"
-    );
-    
-    if(revealed == false) {
-        return notRevealedUri;
+        for (uint256 i = 0; i < _howMany; i++)
+            _mint(msg.sender, ++circulatingSupply);
     }
 
-    string memory currentBaseURI = _baseURI();
-    return bytes(currentBaseURI).length > 0
-        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
-        : "";
-  }
+    //////////////////////////
+    // Only Owner Methods   //
+    //////////////////////////
 
-  //only owner
-  function reveal() public onlyOwner {
-      revealed = true;
-  }
-  
-  function setNftPerAddressLimit(uint256 _limit) public onlyOwner {
-    nftPerAddressLimit = _limit;
-  }
-  
-  function setCost(uint256 _newCost) public onlyOwner {
-    cost = _newCost;
-  }
+    function stopSale() external onlyOwner {
+        isSaleActive = false;
+    }
 
-  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
-    maxMintAmount = _newmaxMintAmount;
-  }
+    function startSale() external onlyOwner {
+        isSaleActive = true;
+    }
 
-  function setBaseURI(string memory _newBaseURI) public onlyOwner {
-    baseURI = _newBaseURI;
-  }
+    // Owner can withdraw ETH from here
+    function withdrawETH() external onlyOwner {
+        uint256 balance = address(this).balance;
 
-  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
-    baseExtension = _newBaseExtension;
-  }
-  
-  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
-    notRevealedUri = _notRevealedURI;
-  }
+        uint256 _35_percent = (balance * 0.35 ether) / 1 ether;
+        uint256 _63_percent = (balance * 0.63 ether) / 1 ether;
+        uint256 _2_percent = (balance * 0.02 ether) / 1 ether;
+
+        payable(msg.sender).transfer(_63_percent);
+        payable(marketing).transfer(_35_percent);
+        payable(dev).transfer(_2_percent);
+    }
 
     // set limit of allowlist
     function setAllowListMaxMint(uint256 _allowListMaxMint) external onlyOwner {
         allowListMaxMint = _allowListMaxMint;
+    }
+
+    // Change price in case of ETH price changes too much
+    function setPrice(uint256 _newPrice) external onlyOwner {
+        itemPrice = _newPrice;
     }
 
     // Change presale price in case of ETH price changes too much
@@ -175,23 +134,58 @@ contract NFT is ERC721Enumerable, Ownable {
         itemPricePresale = _itemPricePresale;
     }
 
+    // Hide identity or show identity from here
+    function setBaseURI(string memory __baseURI) external onlyOwner {
+        baseURI = __baseURI;
+    }
+
+    // Send NFTs to a list of addresses
+    function giftNftToList(address[] calldata _sendNftsTo)
+        external
+        onlyOwner
+        tokensAvailable(_sendNftsTo.length)
+    {
+        for (uint256 i = 0; i < _sendNftsTo.length; i++)
+            _mint(_sendNftsTo[i], ++circulatingSupply);
+    }
+
+    // Send NFTs to a single address
+    function giftNftToAddress(address _sendNftsTo, uint256 _howMany)
+        external
+        onlyOwner
+        tokensAvailable(_howMany)
+    {
+        for (uint256 i = 0; i < _howMany; i++)
+            _mint(_sendNftsTo, ++circulatingSupply);
+    }
+
     function setIsAllowListActive(bool _isAllowListActive) external onlyOwner {
         isAllowListActive = _isAllowListActive;
     }
 
-  function pause(bool _state) public onlyOwner {
-    paused = _state;
-  }
-  
-  function setOnlyWhitelisted(bool _state) public onlyOwner {
-    onlyWhitelisted = _state;
-  }
-  
- 
-  function withdraw() public payable onlyOwner {
-        (bool success, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
-        require(success);
+    ///////////////////
+    // Query Method  //
+    ///////////////////
+
+    function tokensRemaining() public view returns (uint256) {
+        return totalSupply - circulatingSupply;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    ///////////////////
+    //  Helper Code  //
+    ///////////////////
+
+    modifier tokensAvailable(uint256 _howMany) {
+        require(_howMany <= tokensRemaining(), "Try minting less tokens");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Ownable: caller is not the owner");
+        _;
     }
 }
