f2cac34b928814455fa80422b000c6310ffd7b8a
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-08 15:02:09 +0500

    okay

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -1,38 +1,32 @@
-// Hi. If you have any questions or comments in this smart contract please let me know at:
-// Whatsapp +923014440289, Telegram @thinkmuneeb, discord: timon#1213, I'm Muneeb Zubair Khan
-//
-//
-// Smart Contract Made by Muneeb Zubair Khan
-// The UI is made by Abraham Peter, Whatsapp +923004702553, Telegram @Abrahampeterhash.
-//
-//
-//
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
+contract FatCatz is ERC721("Fat Catz", "FC") {
     string public baseURI;
-    bool public isSaleActive;
-    uint256 public circulatingSupply;
-    address public owner = msg.sender;
-    uint256 public itemPrice = 0.08 ether;
-    uint256 public itemPricePresale = 0.06 ether;
-    uint256 public constant totalSupply = 10_333;
 
-    address public marketing = 0x15b5C64EA09dC8cdc1ad31869d2Aa64f5Dd80377;
-    address public dev = 0xc66C9f79AAa0c8E6F3d12C4eFc7D7FE7c1f8B89C;
+    bool public isSaleActive;
+    uint256 public itemPrice = 0.06 ether;
+    
+    uint256 public circulatingSupply;
+    uint256 public constant totalSupply = 9999;
+
+    address public owner = msg.sender;
 
     bool public isAllowListActive;
-    uint256 public allowListMaxMint = 3;
+    uint256 public allowListMaxMint = 2;
+    uint256 public itemPricePresale = 0.04 ether;
+
+    // address => true/false, tells address is in whitelist or not
     mapping(address => bool) public onAllowList;
+
+    // address => how many nfts address minted in presale
     mapping(address => uint256) public allowListClaimedBy;
 
-    ////////////////////
-    //   ALLOWLIST    //
-    ////////////////////
+    //   ALLOWLIST
+    
     function addToAllowList(address[] calldata addresses) external onlyOwner {
         for (uint256 i = 0; i < addresses.length; i++)
             onAllowList[addresses[i]] = true;
@@ -46,9 +40,15 @@ contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
             onAllowList[addresses[i]] = false;
     }
 
-    ////////////////////
-    //    PRESALE     //
-    ////////////////////
+    function stopPresale() external onlyOwner {
+        isAllowListActive = false;
+    }
+
+    function startPresale() external onlyOwner {
+        isAllowListActive = true;
+    }
+
+    //    PRESALE
 
     // Purchase multiple NFTs at once
     function purchasePresaleTokens(uint256 _howMany)
@@ -58,14 +58,10 @@ contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
     {
         require(isAllowListActive, "Allowlist is not active");
         require(onAllowList[msg.sender], "You are not in allowlist");
-        require(
-            allowListClaimedBy[msg.sender] + _howMany <= allowListMaxMint,
+        require(allowListClaimedBy[msg.sender] + _howMany <= allowListMaxMint,
             "Purchase exceeds max allowed"
         );
-        require(
-            msg.value >= _howMany * itemPricePresale,
-            "Try to send more ETH"
-        );
+        require(msg.value >= _howMany * itemPricePresale, "Try to send more ETH");
 
         allowListClaimedBy[msg.sender] += _howMany;
 
@@ -73,9 +69,7 @@ contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
             _mint(msg.sender, ++circulatingSupply);
     }
 
-    ////////////////////
-    //  PUBLIC SALE   //
-    ////////////////////
+    //  PUBLIC SALE
 
     // Purchase multiple NFTs at once
     function purchaseTokens(uint256 _howMany)
@@ -83,10 +77,7 @@ contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
         payable
         tokensAvailable(_howMany)
     {
-        require(
-            isSaleActive,
-            "Sale is not active"
-        );
+        require(isSaleActive, "Sale is not active");
         require(_howMany > 0 && _howMany <= 10, "Mint min 1, max 10");
         require(msg.value >= _howMany * itemPrice, "Try to send more ETH");
 
@@ -94,10 +85,7 @@ contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
             _mint(msg.sender, ++circulatingSupply);
     }
 
-    //////////////////////////
-    // Only Owner Methods   //
-    //////////////////////////
-
+    // Only Owner Methods
     function stopSale() external onlyOwner {
         isSaleActive = false;
     }
@@ -106,33 +94,21 @@ contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
         isSaleActive = true;
     }
 
-    // Owner can withdraw ETH from here
-    function withdrawETH() external onlyOwner {
-        uint256 balance = address(this).balance;
-
-        uint256 _35_percent = (balance * 0.35 ether) / 1 ether;
-        uint256 _63_percent = (balance * 0.63 ether) / 1 ether;
-        uint256 _2_percent = (balance * 0.02 ether) / 1 ether;
-
-        payable(msg.sender).transfer(_63_percent);
-        payable(marketing).transfer(_35_percent);
-        payable(dev).transfer(_2_percent);
-    }
-
     // set limit of allowlist
     function setAllowListMaxMint(uint256 _allowListMaxMint) external onlyOwner {
         allowListMaxMint = _allowListMaxMint;
     }
 
+    // Change presale price in case of ETH price changes too much
+    function setItemPricePresale(uint256 _itemPricePresale) external onlyOwner {
+        itemPricePresale = _itemPricePresale;
+    }
+
     // Change price in case of ETH price changes too much
-    function setPrice(uint256 _newPrice) external onlyOwner {
+    function setItemPrice(uint256 _newPrice) external onlyOwner {
         itemPrice = _newPrice;
     }
 
-    // Change presale price in case of ETH price changes too much
-    function setPricePresale(uint256 _itemPricePresale) external onlyOwner {
-        itemPricePresale = _itemPricePresale;
-    }
 
     // Hide identity or show identity from here
     function setBaseURI(string memory __baseURI) external onlyOwner {
@@ -140,7 +116,7 @@ contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
     }
 
     // Send NFTs to a list of addresses
-    function giftNftToList(address[] calldata _sendNftsTo)
+    function giftNftsToList(address[] calldata _sendNftsTo)
         external
         onlyOwner
         tokensAvailable(_sendNftsTo.length)
@@ -150,7 +126,7 @@ contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
     }
 
     // Send NFTs to a single address
-    function giftNftToAddress(address _sendNftsTo, uint256 _howMany)
+    function giftNftsToSingleAddress(address _sendNftsTo, uint256 _howMany)
         external
         onlyOwner
         tokensAvailable(_howMany)
@@ -159,33 +135,26 @@ contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
             _mint(_sendNftsTo, ++circulatingSupply);
     }
 
-    function setIsAllowListActive(bool _isAllowListActive) external onlyOwner {
-        isAllowListActive = _isAllowListActive;
-    }
-
-    ///////////////////
-    // Query Method  //
-    ///////////////////
-
-    function tokensRemaining() public view returns (uint256) {
-        return totalSupply - circulatingSupply;
-    }
+    // Query Method
 
     function _baseURI() internal view override returns (string memory) {
         return baseURI;
     }
 
-    ///////////////////
-    //  Helper Code  //
-    ///////////////////
+    // Only owner can withdraw from this method
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    //  Utility Code
 
     modifier tokensAvailable(uint256 _howMany) {
-        require(_howMany <= tokensRemaining(), "Try minting less tokens");
+        require(_howMany <= totalSupply - circulatingSupply, "Try minting less tokens");
         _;
     }
 
     modifier onlyOwner() {
-        require(owner == msg.sender, "Ownable: caller is not the owner");
+        require(owner == msg.sender, "Caller is not the owner");
         _;
     }
 }
