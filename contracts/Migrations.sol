50caa221ce32311f7b1e7522eed9ee253cb1ca40
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-16 03:17:53 +0500

    ok

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,12 +1,4 @@
-// Hi. If you have any questions or comments in this smart contract please let me know at:
-// muneeb.zubair.hash@gmail.com, Whatsapp +923014440289, Telegram @thinkmuneeb, discord: timon#1213, I'm Muneeb Zubair Khan
-//
-//
-// Smart Contract Made by Muneeb Zubair Khan
-// The UI is made by Abraham Peter, abraham.peter.hash@gmail.com, Whatsapp +923004702553, Telegram @Abrahampeterhash.
-//
-//
-//
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
