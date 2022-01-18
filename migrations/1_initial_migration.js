6520b5c1b4d91d7ebc298764d9bd5bfe89ec9268
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-18 18:05:14 +0500

    ok

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -5,8 +5,8 @@ const { makeHelperCodeForUIDev: makeUiCode } = require('./helper');
 module.exports = async (deployer, network, accounts) => {
   // if (network === "development") return;
 
-  await deployer.deploy(Migrations);
+  // await deployer.deploy(Migrations);
   const nftStaking = await deployer.deploy(NftStaking);
   // const nft = await deployer.deploy(NftStaking, { from: accounts[1] });
-  makeUiCode(network, { nftStaking });
+  // makeUiCode(network, { nftStaking });
 };;
