087e46d36311b6aa5ec3a040e7399cd02d19e3d3
Muneeb <muneeb.zubair.hash@gmail.com>
2022-01-30 20:40:42 +0500

    ok

--- test/PresaleFactory.test.js
+++ test/PresaleFactory.test.js
@@ -2,7 +2,7 @@ const { makeUiCode } = require("../migrations/helper.js");
 
 const Nft = artifacts.require("LazyPandas_Advanced");
 
-contract("Presale Generator", async ([owner, client, parentCompany]) => {
+contract("Nft", async ([owner, client, parentCompany]) => {
   it("deploy smart contract", async () => {
     //
     let nft = await Nft.new({ from: owner });
