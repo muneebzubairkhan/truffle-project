c37570ae21a77766b1cbe0c32f8655aa5f47e449
Muneeb <muneeb.zubair.hash@gmail.com>
2022-02-09 20:26:37 +0500

    merkle tree added

---
+++ contracts/ERC721_Alternate.sol
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract DysfunctionalDogs is ERC721A("DysfunctionalDogs", "DDs"), Ownable {
+    using Strings for uint256;
+
+    string public baseURI;
+    string public baseExtension = ".json";
+    string public notRevealedUri;
+    uint256 public cost = 0.075 ether;
+    uint256 public maxSupply = 10000;
+    uint256 public maxMintAmount = 20;
+    uint256 public nftPerAddressLimit = 3;
+    bool public publicmintActive = true;
+    bool public revealed = false;
+
+    // internal
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    // public
+    function mint(uint256 _mintAmount) public payable {
+        require(publicmintActive, "the contract is paused");
+        uint256 supply = totalSupply();
+        require(_mintAmount > 0, "need to mint at least 1 NFT");
+        require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
+        require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+        require(msg.value >= cost * _mintAmount, "insufficient funds");
+
+        _safeMint(msg.sender, _mintAmount);
+    }
+
+    function walletOfOwner(address _owner) public view returns (uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        for (uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        if (revealed == false) {
+            return notRevealedUri;
+        }
+
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length > 0 ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension)) : "";
+    }
+
+    //only owner
+    function reveal() public onlyOwner {
+        revealed = true;
+    }
+
+    function setNftPerAddressLimit(uint256 _limit) public onlyOwner {
+        nftPerAddressLimit = _limit;
+    }
+
+    function setCost(uint256 _newCost) public onlyOwner {
+        cost = _newCost;
+    }
+
+    function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+        maxMintAmount = _newmaxMintAmount;
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+        baseExtension = _newBaseExtension;
+    }
+
+    function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+        notRevealedUri = _notRevealedURI;
+    }
+
+    function publicMint(bool _state) public onlyOwner {
+        publicmintActive = _state;
+    }
+
+    function withdraw() public payable onlyOwner {
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success);
+    }
+
+    ///////////////////////////////////
+    //       AIRDROP CODE STARTS     //
+    ///////////////////////////////////
+
+    // Send NFTs to a list of addresses
+    function giftNftToList(address[] calldata _sendNftsTo) external onlyOwner {
+        require(totalSupply() + _sendNftsTo.length <= maxSupply, "max NFT limit exceeded");
+
+        for (uint256 i = 0; i < _sendNftsTo.length; i++) _safeMint(_sendNftsTo[i], 1);
+    }
+
+    // Send NFTs to a single address
+    function giftNftToAddress(address _sendNftsTo, uint256 _howMany) external onlyOwner {
+        require(totalSupply() + _howMany <= maxSupply, "max NFT limit exceeded");
+
+        _safeMint(_sendNftsTo, _howMany);
+    }
+
+    ///////////////////////////////
+    //    PRESALE CODE STARTS    //
+    ///////////////////////////////
+
+    uint256 public itemPricePresale = 0.03 ether;
+    uint256 public presaleActiveTime;
+    uint256 public presaleMaxMint = 3;
+    mapping(address => uint256) public presaleClaimedBy;
+    bytes32 public whitelistMerkleRoot;
+
+    function setWhitelistMerkleRoot(bytes32 _whitelistMerkleRoot) external onlyOwner {
+        whitelistMerkleRoot = _whitelistMerkleRoot;
+    }
+
+    function inWhitelist(bytes32[] memory _proof, address _owner) public view returns (bool) {
+        return MerkleProof.verify(_proof, whitelistMerkleRoot, keccak256(abi.encodePacked(_owner)));
+    }
+
+    function purchasePresaleTokens(uint256 _howMany, bytes32[] calldata _proof) external payable {
+        uint256 supply = totalSupply();
+        require(supply + _howMany <= maxSupply, "max NFT limit exceeded");
+
+        require(inWhitelist(_proof, msg.sender), "You are not in presale");
+        require(block.timestamp > presaleActiveTime, "Presale is not active");
+        require(msg.value >= _howMany * itemPricePresale, "Try to send more ETH");
+
+        presaleClaimedBy[msg.sender] += _howMany;
+
+        require(presaleClaimedBy[msg.sender] <= presaleMaxMint, "Purchase exceeds max allowed");
+
+        _safeMint(msg.sender, _howMany);
+    }
+
+    // set limit of presale
+    function setPresaleMaxMint(uint256 _presaleMaxMint) external onlyOwner {
+        presaleMaxMint = _presaleMaxMint;
+    }
+
+    // Change presale price in case of ETH price changes too much
+    function setPricePresale(uint256 _itemPricePresale) external onlyOwner {
+        itemPricePresale = _itemPricePresale;
+    }
+
+    function setPresaleActiveTime(uint256 _presaleActiveTime) external onlyOwner {
+        presaleActiveTime = _presaleActiveTime;
+    }
+}
