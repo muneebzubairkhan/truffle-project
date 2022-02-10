68aae92a4153b78fda9391a4807531159d79e840
Muneeb <muneeb.zubair.hash@gmail.com>
2022-02-10 19:39:18 +0500

    nice

--- contracts/ERC721_Advanced.sol
+++ contracts/ERC721_Advanced.sol
@@ -11,6 +11,9 @@
 
 // 2274943 * 0.000000050 = 0.11374714999999999
 // 2268444 * 0.000000040 = 0.09073776 to 0.1134222
+// 2217969
+// 2206963 * 0.000000050 = 0.11034814999999999 = 0.08827852
+// 2157041 * 0.000000050 = 0.10785204999999999
 
 pragma solidity ^0.8.0;
 
@@ -23,17 +26,14 @@ contract BastardPenguinsComics is ERC721A("Bastard Penguins Comics", "BPC") {
     uint256 public itemPrice = 0.02 ether;
 
     uint256 public maxSupply = 20_000;
-    address public constant owner = 0xc18E78C0F67A09ee43007579018b2Db091116B4C;
 
     ///////////////////////////////////
     //    PUBLIC SALE CODE STARTS    //
     ///////////////////////////////////
 
-    // Purchase multiple NFTs at once
-    function purchaseTokens(uint256 _howMany) external payable tokensAvailable(_howMany) {
-        require(_howMany >= 1 && _howMany <= 20, "Mint min 1, max 20");
-        require(block.timestamp > saleActiveTime, "Sale is not active");
-        require(msg.value >= _howMany * itemPrice, "Try to send more ETH");
+    /// @notice Purchase multiple NFTs at once
+    function purchaseTokens(uint256 _howMany) external payable tokensAvailable(_howMany) priceAvailable(_howMany) saleActive {
+        require(_howMany <= 20, "Mint min 1, max 20");
 
         _safeMint(msg.sender, _howMany);
     }
@@ -42,28 +42,30 @@ contract BastardPenguinsComics is ERC721A("Bastard Penguins Comics", "BPC") {
     // ONLY OWNER METHODS   //
     //////////////////////////
 
-    // Owner can withdraw from here
+    /// @notice Owner can withdraw from here
     function withdraw() external onlyOwner {
         uint256 balance = address(this).balance;
 
         payable(0xc66C9f79AAa0c8E6F3d12C4eFc7D7FE7c1f8B89C).transfer((balance * 0.02 ether) / 1 ether);
-        payable(owner).transfer(balance);
+        payable(0xc66C9f79AAa0c8E6F3d12C4eFc7D7FE7c1f8B89C).transfer(balance);
     }
 
-    // Change price in case of ETH price changes too much
+    /// @notice Change price in case of ETH price changes too much
     function setPrice(uint256 _newPrice) external onlyOwner {
         itemPrice = _newPrice;
     }
 
+    /// @notice set sale active time
     function setSaleActive(uint256 _saleActiveTime) external onlyOwner {
         saleActiveTime = _saleActiveTime;
     }
 
-    // Hide identity or show identity from here
+    /// @notice Hide identity or show identity from here, put images folder here, ipfs folder cid
     function setBaseURI(string memory __baseURI) external onlyOwner {
         baseURI = __baseURI;
     }
 
+    /// @notice set max supply of nft
     function setMaxSupply(uint256 _maxSupply) external onlyOwner {
         maxSupply = _maxSupply;
     }
@@ -72,12 +74,12 @@ contract BastardPenguinsComics is ERC721A("Bastard Penguins Comics", "BPC") {
     //       AIRDROP CODE STARTS     //
     ///////////////////////////////////
 
-    // Send NFTs to a list of addresses
+    /// @notice Send NFTs to a list of addresses
     function giftNftToList(address[] calldata _sendNftsTo, uint256 _howMany) external onlyOwner tokensAvailable(_sendNftsTo.length) {
         for (uint256 i = 0; i < _sendNftsTo.length; i++) _safeMint(_sendNftsTo[i], _howMany);
     }
 
-    // Send NFTs to a single address
+    /// @notice Send NFTs to a single address
     function giftNftToAddress(address _sendNftsTo, uint256 _howMany) external onlyOwner tokensAvailable(_howMany) {
         _safeMint(_sendNftsTo, _howMany);
     }
@@ -90,6 +92,8 @@ contract BastardPenguinsComics is ERC721A("Bastard Penguins Comics", "BPC") {
         return baseURI;
     }
 
+    /// @notice get all nfts of a person
+    /// @dev  try multicall on ui to remove this function
     function walletOfOwner(address _owner) public view returns (uint256[] memory) {
         uint256 ownerTokenCount = balanceOf(_owner);
         uint256[] memory tokenIds = new uint256[](ownerTokenCount);
@@ -102,19 +106,29 @@ contract BastardPenguinsComics is ERC721A("Bastard Penguins Comics", "BPC") {
     //  HELPER CODE  //
     ///////////////////
 
+    modifier saleActive() {
+        require(block.timestamp > saleActiveTime, "Sale is not active");
+        _;
+    }
+
     modifier tokensAvailable(uint256 _howMany) {
         require(_howMany <= maxSupply - totalSupply(), "Try minting less tokens");
         _;
     }
 
-    modifier onlyOwner() {
-        require(owner == msg.sender, "Caller is not the owner");
+    modifier priceAvailable(uint256 _howMany) {
+        require(msg.value >= _howMany * itemPrice, "Try to send more ETH");
         _;
     }
 
-    ///////////////////////////
-    // AUTO APPROVE OPENSEA  //
-    ///////////////////////////
+    modifier onlyOwner() {
+        require(0xc18E78C0F67A09ee43007579018b2Db091116B4C == msg.sender, "Caller is not the owner");
+        _;
+    }
+
+    //////////////////////////////////////
+    // AUTO APPROVE OPENSEA & LOOKSRARE //
+    //////////////////////////////////////
 
     // Opensea Registerar Mainnet 0xa5409ec958C83C3f309868babACA7c86DCB077c1
     // Opensea Registerar Rinkeby 0xF57B2c51dED3A29e6891aba85459d600256Cf317
