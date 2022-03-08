197fdc96e136c9b7557f50ab07229a697fa9b1a7
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-08 21:22:50 +0500

    ok

--- contracts/ERC721_Advanced.sol
+++ contracts/ERC721_Advanced.sol
@@ -7,33 +7,52 @@
 // Opensea:  https://opensea.io/assets/0xb94b38fcb227350989f95f54f54f43b5fcc3ccff/0
 // Contract: https://etherscan.io/address/0xb94b38fcb227350989f95f54f54f43b5fcc3ccff#code
 
+// ########  #######  ##     ##      ##    ##    ###    ######## ####  #######  ##    ##      ########     ###     #######
+// ##       ##     ##  ##   ##       ###   ##   ## ##      ##     ##  ##     ## ###   ##      ##     ##   ## ##   ##     ##
+// ##       ##     ##   ## ##        ####  ##  ##   ##     ##     ##  ##     ## ####  ##      ##     ##  ##   ##  ##     ##
+// ######   ##     ##    ###         ## ## ## ##     ##    ##     ##  ##     ## ## ## ##      ##     ## ##     ## ##     ##
+// ##       ##     ##   ## ##        ##  #### #########    ##     ##  ##     ## ##  ####      ##     ## ######### ##     ##
+// ##       ##     ##  ##   ##       ##   ### ##     ##    ##     ##  ##     ## ##   ###      ##     ## ##     ## ##     ##
+// ##        #######  ##     ##      ##    ## ##     ##    ##    ####  #######  ##    ##      ########  ##     ##  #######
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-
+//pause nfts
 import "erc721a/contracts/ERC721A.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "erc721a/contracts/extensions/ERC721ABurnable.sol";
 
-contract UAC is ERC721A("Underground Ape Club", "UAC"), Pausable {
-    string public baseURI = "ipfs://QmVTNcKHkqF9LBAKsUJ5AjuRzNMLGwpgqmtE445drcktnx/";
+interface OpenSea {
+    function proxies(address) external view returns (address);
+}
 
-    uint256 public saleActiveTime = block.timestamp + 30 seconds;
-    uint256 public itemPrice = 0.06 ether;
+contract FoxNationDAO is ERC721A("FoxNationDAO", "FNDAO"), ERC721ABurnable, ERC2981, Ownable {
+    uint256 public maxSupply = 10_000;
+    uint256 public itemPrice = 0.07 ether;
+    uint256 public saleActiveTime = block.timestamp + 365 days;
+    string public baseURI;
 
-    uint256 public constant maxSupply = 10000;
-    address public constant owner = 0xc18E78C0F67A09ee43007579018b2Db091116B4C;
+    constructor() {
+        _setDefaultRoyalty(msg.sender, 7_50); // 7.50 %
+    }
 
     ///////////////////////////////////
     //    PUBLIC SALE CODE STARTS    //
     ///////////////////////////////////
 
-    // Purchase multiple NFTs at once
-    function purchaseTokens(uint256 _howMany) external payable callerIsUser tokensAvailable(_howMany) {
-        require(block.timestamp > saleActiveTime, "Sale is not active");
-        require(_howMany >= 1 && _howMany <= 20, "Mint min 1, max 20");
-        require(msg.value >= _howMany * itemPrice, "Try to send more ETH");
-
+    /// @notice Purchase multiple NFTs at once
+    function purchaseTokens(uint256 _howMany)
+        external
+        payable
+        saleActive
+        callerIsUser
+        mintLimit(_howMany)
+        priceAvailable(_howMany)
+        tokensAvailable(_howMany)
+    {
         _safeMint(msg.sender, _howMany);
     }
 
@@ -41,212 +60,202 @@ contract UAC is ERC721A("Underground Ape Club", "UAC"), Pausable {
     // ONLY OWNER METHODS   //
     //////////////////////////
 
-    // Owner can withdraw from here
+    /// @notice Owner can withdraw from here
     function withdraw() external onlyOwner {
-        uint256 balance = address(this).balance;
-
-        payable(0xc66C9f79AAa0c8E6F3d12C4eFc7D7FE7c1f8B89C).transfer((balance * 0.02 ether) / 1 ether);
-        payable(owner).transfer(balance);
+        payable(msg.sender).transfer(address(this).balance);
     }
 
-    // Change price in case of ETH price changes too much
+    /// @notice Change price in case of ETH price changes too much
     function setPrice(uint256 _newPrice) external onlyOwner {
         itemPrice = _newPrice;
     }
 
-    function setSaleActive(uint256 _saleActiveTime) external onlyOwner {
+    /// @notice set sale active time
+    function setSaleActiveTime(uint256 _saleActiveTime) external onlyOwner {
         saleActiveTime = _saleActiveTime;
     }
 
-    // Hide identity or show identity from here
+    /// @notice Hide identity or show identity from here, put images folder here, ipfs folder cid
     function setBaseURI(string memory __baseURI) external onlyOwner {
         baseURI = __baseURI;
+    }
+
+    /// @notice set max supply of nft
+    function setMaxSupply(uint256 _maxSupply) external onlyOwner {
+        maxSupply = _maxSupply;
     }
 
     ///////////////////////////////////
     //       AIRDROP CODE STARTS     //
     ///////////////////////////////////
 
-    // Send NFTs to a list of addresses
-    function giftNftToList(address[] calldata _sendNftsTo, uint256 _howMany) external onlyOwner tokensAvailable(_sendNftsTo.length) {
+    /// @notice Send NFTs to a list of addresses
+    function giftNft(address[] calldata _sendNftsTo, uint256 _howMany) external onlyOwner tokensAvailable(_sendNftsTo.length * _howMany) {
         for (uint256 i = 0; i < _sendNftsTo.length; i++) _safeMint(_sendNftsTo[i], _howMany);
     }
 
-    // Send NFTs to a single address
-    function giftNftToAddress(address _sendNftsTo, uint256 _howMany) external onlyOwner tokensAvailable(_howMany) {
-        _safeMint(_sendNftsTo, _howMany);
-    }
+    ////////////////////
+    // HELPER METHOD  //
+    ////////////////////
 
-    ///////////////////
-    // QUERY METHOD  //
-    ///////////////////
-
-    uint256 reserveTokens = 200;
-
-    function setReserveTokens(uint256 num) public onlyOwner {
-        reserveTokens = num;
-    }
-
-    function tokensRemaining() public view returns (uint256) {
-        return maxSupply - totalSupply() - reserveTokens;
-    }
-
-    function _baseURI() internal view override returns (string memory) {
-        return baseURI;
-    }
-
-    function walletOfOwner(address _owner) public view returns (uint256[] memory) {
+    /// @notice get all nfts of a person
+    function walletOfOwner(address _owner) external view returns (uint256[] memory) {
         uint256 ownerTokenCount = balanceOf(_owner);
         uint256[] memory tokenIds = new uint256[](ownerTokenCount);
         for (uint256 i; i < ownerTokenCount; i++) tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
-
         return tokenIds;
+    }
+
+    function tokenOfOwnerByIndex(address _owner, uint256 index) public view returns (uint256) {
+        if (index >= balanceOf(_owner)) revert();
+        uint256 numMintedSoFar = _currentIndex;
+        uint256 tokenIdsIdx;
+        address currOwnershipAddr;
+        unchecked {
+            for (uint256 i; i < numMintedSoFar; i++) {
+                TokenOwnership memory ownership = _ownerships[i];
+                if (ownership.burned) continue;
+                if (ownership.addr != address(0)) currOwnershipAddr = ownership.addr;
+                if (currOwnershipAddr == _owner) {
+                    if (tokenIdsIdx == index) return i;
+                    tokenIdsIdx++;
+                }
+            }
+        }
+        revert();
     }
 
     ///////////////////
     //  HELPER CODE  //
     ///////////////////
 
-    modifier tokensAvailable(uint256 _howMany) {
-        require(_howMany <= tokensRemaining(), "Try minting less tokens");
-        _;
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
     }
 
-    modifier onlyOwner() {
-        require(owner == msg.sender, "Caller is not the owner");
-        _;
-    }
-    
     modifier callerIsUser() {
-        require(tx.origin == msg.sender, "The caller is another contract");
+        require(tx.origin == msg.sender, "The caller is a contract");
         _;
     }
 
-    //////////////////////////////
-    // WHITELISTING FOR STAKING //
-    //////////////////////////////
-
-    // address => can stake (yes or no)
-    mapping(address => bool) public canStake;
-
-    // add / remove from whitelist who can stake / unstake
-    function addToCanStake(address _address, bool _add) external onlyOwner {
-        canStake[_address] = _add;
-    }
-
-    modifier onlyCanStake() {
-        require(canStake[msg.sender], "Caller can not stake");
+    modifier saleActive() {
+        require(block.timestamp > saleActiveTime, "Sale is not active");
         _;
     }
 
-    /////////////////////
-    // STAKING METHOD  //
-    /////////////////////
-
-    mapping(uint256 => bool) public staked;
-
-    function _beforeTokenTransfers(
-        address,
-        address,
-        uint256 startTokenId,
-        uint256 quantity
-    ) internal view override whenNotPaused {
-        // can improve it, give reward on balanceOf()
-        // try gas by saparating contracts
-        for (uint256 i = startTokenId; i < startTokenId + quantity; i++) require(!staked[i], "Unstake tokenId it to transfer");
+    modifier mintLimit(uint256 _howMany) {
+        require(_howMany >= 1 && _howMany <= 20, "Mint min 1, max 20");
+        _;
     }
 
-    // stake / unstake nfts
-    function stakeNfts(uint256[] calldata _tokenIds, bool _stake) external onlyCanStake {
-        for (uint256 i = 0; i < _tokenIds.length; i++) staked[_tokenIds[i]] = _stake;
+    modifier tokensAvailable(uint256 _howMany) {
+        require(_howMany <= maxSupply - totalSupply(), "Try minting less tokens");
+        _;
     }
 
-    /////////////////////////////
-    // AUTO APPROVE LOOKSRARE  //
-    /////////////////////////////
+    modifier priceAvailable(uint256 _howMany) {
+        require(msg.value >= _howMany * itemPrice, "Try to send more ETH");
+        _;
+    }
 
-    // TransferManagerERC721 0xf42aa99F011A1fA7CDA90E5E98b277E306BcA83e
-    // LooksRareExchange 0x59728544b08ab483533076417fbbb2fd0b17ce3a
+    ///////////////////////////////
+    // AUTO APPROVE MARKETPLACES //
+    ///////////////////////////////
 
-    ///////////////////////////
-    // AUTO APPROVE OPENSEA  //
-    ///////////////////////////
+    mapping(address => bool) public projectProxy; // check public vs private vs internal gas
 
-    // Opensea Registerar Mainnet 0xa5409ec958C83C3f309868babACA7c86DCB077c1
-    // Opensea Registerar Rinkeby 0xF57B2c51dED3A29e6891aba85459d600256Cf317
+    function flipProxyState(address proxyAddress) public onlyOwner {
+        projectProxy[proxyAddress] = !projectProxy[proxyAddress];
+    }
 
     function isApprovedForAll(address _owner, address _operator) public view override returns (bool) {
-        return OpenSea(0xa5409ec958C83C3f309868babACA7c86DCB077c1).proxies(_owner) == _operator ? true : super.isApprovedForAll(_owner, _operator);
+        // OPENSEA
+        if (_operator == OpenSea(0xa5409ec958C83C3f309868babACA7c86DCB077c1).proxies(_owner)) return true;
+        // LOOKSRARE
+        else if (_operator == 0xf42aa99F011A1fA7CDA90E5E98b277E306BcA83e) return true;
+        // RARIBLE
+        else if (_operator == 0x4feE7B061C97C9c496b01DbcE9CDb10c02f0a0Be) return true;
+        // X2Y2
+        else if (_operator == 0xF849de01B080aDC3A814FaBE1E2087475cF2E354) return true;
+        // ANY OTHER Marketpalce
+        else if (projectProxy[_operator]) return true;
+        return super.isApprovedForAll(_owner, _operator);
     }
 
-    ///////////////////////////
-    //    PAUSE NFT SALES    //
-    ///////////////////////////
-
-    function pause() public onlyOwner {
-        _pause();
+    /// @notice _startTokenId from 1 not 0
+    function _startTokenId() internal pure override returns (uint256) {
+        return 1;
     }
 
-    function unpause() public onlyOwner {
-        _unpause();
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721A, ERC2981) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
-    // send multiple nfts
-    function bulkAirdropERC721(
-        IERC721 _token,
-        address[] calldata _to,
-        uint256[] calldata _id
-    ) public {
-        require(_to.length == _id.length, "Receivers and IDs are different length");
-        for (uint256 i = 0; i < _to.length; i++) {
-            _token.safeTransferFrom(msg.sender, _to[i], _id[i]);
-        }
+    function burn(uint256 tokenId) public override {
+        super._burn(tokenId);
+        _resetTokenRoyalty(tokenId);
+    }
+
+    function setDefaultRoyalty(address _receiver, uint96 _feeNumerator) public onlyOwner {
+        _setDefaultRoyalty(_receiver, _feeNumerator);
     }
 }
 
-interface OpenSea {
-    function proxies(address) external view returns (address);
-}
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract PresaleUAC is UAC {
-    ///////////////////////////////
-    //    PRESALE CODE STARTS    //
-    ///////////////////////////////
+contract FoxNationDAOPresale is FoxNationDAO {
+    // multiple presale configs
+    mapping(uint256 => uint256) public maxMintPresales;
+    mapping(uint256 => uint256) public itemPricePresales;
+    mapping(uint256 => bytes32) public whitelistMerkleRoots;
+    uint256 public presaleActiveTime = block.timestamp + 365 days;
 
-    uint256 public itemPricePresale = 0.03 ether;
-    uint256 public presaleActiveTime;
-    uint256 public presaleMaxMint = 3;
-    mapping(address => uint256) public presaleClaimedBy;
-    bytes32 public whitelistMerkleRoot;
-
-    function setWhitelistMerkleRoot(bytes32 _whitelistMerkleRoot) external onlyOwner {
-        whitelistMerkleRoot = _whitelistMerkleRoot;
+    // multicall inWhitelist
+    function inWhitelist(
+        address _owner,
+        bytes32[] memory _proof,
+        uint256 _from,
+        uint256 _to
+    ) external view returns (uint256) {
+        for (uint256 i = _from; i < _to; i++) if (_inWhitelist(_owner, _proof, i)) return i;
+        return type(uint256).max;
     }
 
-    function inWhitelist(bytes32[] memory _proof, address _owner) public view returns (bool) {
-        return MerkleProof.verify(_proof, whitelistMerkleRoot, keccak256(abi.encodePacked(_owner)));
+    function _inWhitelist(
+        address _owner,
+        bytes32[] memory _proof,
+        uint256 _rootNumber
+    ) private view returns (bool) {
+        return MerkleProof.verify(_proof, whitelistMerkleRoots[_rootNumber], keccak256(abi.encodePacked(_owner)));
     }
 
-    function purchasePresaleTokens(uint256 _howMany, bytes32[] calldata _proof) external payable callerIsUser tokensAvailable(_howMany)  {
-        require(inWhitelist(_proof, msg.sender), "You are not in presale");
+    function purchasePresaleTokens(
+        uint256 _howMany,
+        bytes32[] calldata _proof,
+        uint256 _rootNumber
+    ) external payable callerIsUser tokensAvailable(_howMany) {
         require(block.timestamp > presaleActiveTime, "Presale is not active");
-        require(msg.value >= _howMany * itemPricePresale, "Try to send more ETH");
-
-        presaleClaimedBy[msg.sender] += _howMany;
-
-        require(presaleClaimedBy[msg.sender] <= presaleMaxMint, "Purchase exceeds max allowed");
+        require(_inWhitelist(msg.sender, _proof, _rootNumber), "You are not in presale");
+        require(msg.value >= _howMany * itemPricePresales[_rootNumber], "Try to send more ETH");
 
         _safeMint(msg.sender, _howMany);
+
+        require(_numberMinted(msg.sender) <= maxMintPresales[_rootNumber], "Purchase exceeds max allowed");
     }
 
-    // set limit of presale
-    function setPresaleMaxMint(uint256 _presaleMaxMint) external onlyOwner {
-        presaleMaxMint = _presaleMaxMint;
+    function numberMinted(address _owner) external view returns (uint256) {
+        return _numberMinted(_owner);
     }
 
-    // Change presale price in case of ETH price changes too much
-    function setPricePresale(uint256 _itemPricePresale) external onlyOwner {
-        itemPricePresale = _itemPricePresale;
+    function setPresale(
+        uint256 _rootNumber,
+        bytes32 _whitelistMerkleRoot,
+        uint256 _maxMintPresales,
+        uint256 _itemPricePresale
+    ) external onlyOwner {
+        maxMintPresales[_rootNumber] = _maxMintPresales;
+        itemPricePresales[_rootNumber] = _itemPricePresale;
+        whitelistMerkleRoots[_rootNumber] = _whitelistMerkleRoot;
     }
 
     function setPresaleActiveTime(uint256 _presaleActiveTime) external onlyOwner {
@@ -254,4 +263,50 @@ contract PresaleUAC is UAC {
     }
 }
 
-// 2860548 * 0.000000050 = 0.1430274
+contract FoxNationDAOERC20 is FoxNationDAOPresale {
+    /// @notice set itemPrice in Erc20
+    uint256 public itemPriceErc20 = 200 ether;
+    uint256 public saleActiveTimeErc20 = block.timestamp + 365 days;
+    address public erc20 = 0xc3D6F4b97292f8d48344B36268BDd7400180667E; // To Buy In Token, USDT Token
+
+    /// @notice Purchase multiple NFTs at once
+    function purchaseTokensErc20(uint256 _howMany)
+        external
+        callerIsUser
+        saleActiveErc20
+        mintLimit(_howMany)
+        tokensAvailable(_howMany)
+        priceAvailableERC20(_howMany)
+    {
+        _safeMint(msg.sender, _howMany);
+    }
+
+    /// @notice Owner can withdraw from here
+    function withdrawERC20(address _erc20) external onlyOwner {
+        IERC20(_erc20).transferFrom(address(this), msg.sender, IERC20(_erc20).balanceOf(address(this)));
+    }
+
+    function setErc20(address _erc20) external onlyOwner {
+        erc20 = _erc20;
+    }
+
+    /// @notice set itemPrice in Erc20
+    function setItemPriceErc20(uint256 _itemPriceErc20) external onlyOwner {
+        itemPriceErc20 = _itemPriceErc20;
+    }
+
+    /// @notice set sale active time
+    function setSaleActiveTimeERC20(uint256 _saleActiveTimeErc20) external onlyOwner {
+        saleActiveTimeErc20 = _saleActiveTimeErc20;
+    }
+
+    modifier saleActiveErc20() {
+        require(block.timestamp > saleActiveTimeErc20, "Sale is not active");
+        _;
+    }
+
+    modifier priceAvailableERC20(uint256 _howMany) {
+        require(IERC20(erc20).transferFrom(msg.sender, address(this), _howMany * itemPriceErc20), "Try to send more ERC20");
+        _;
+    }
+}
