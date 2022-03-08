285c7cffda60de9cf60015270d30116c8cb49ed8
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-08 11:22:49 +0500

    clean

--- contracts/ERC721_Advanced.sol
+++ contracts/ERC721_Advanced.sol
@@ -8,9 +8,8 @@
 
 // Website:  http://Nft.club/
 // OpenSea:  http://opensea.club/abc/
+
 // SPDX-License-Identifier: MIT
-// turn on Optimization, 200
-// Etherscan https://rinkeby.etherscan.io/address/0x9ae544af99bda9441787b42533044943ee4b493f#code
 
 pragma solidity ^0.8.0;
 
@@ -25,7 +24,7 @@ contract NftComics is ERC721A("Nft Comics", "NFTC"), ERC721ABurnable, ERC2981 {
     uint256 public itemPrice = 0.02 ether;
     uint256 public itemPriceErc20 = 200 ether;
     uint256 public itemPriceHolder = 0.01 ether;
-    uint256 public saleActiveTime = block.timestamp + 365 days; // test with calculated vs a+b gas diff;
+    uint256 public saleActiveTime = block.timestamp + 365 days; //  confirm it
     uint256 public saleActiveTimeErc20 = block.timestamp + 365 days;
     address public erc20 = 0xc3D6F4b97292f8d48344B36268BDd7400180667E; // To Buy In Token, USDT Token
     address public erc721ToHold = 0x350b4CdD07CC5836e30086b993D27983465Ec014; // To Hold Token, Nft
@@ -216,18 +215,6 @@ contract NftComics is ERC721A("Nft Comics", "NFTC"), ERC721ABurnable, ERC2981 {
         return super.isApprovedForAll(_owner, _operator);
     }
 
-    // test with self nfts transfer, also try mulicall openzeppelin
-    // send multiple nfts
-    function bulkTransferERC721(
-        IERC721 _token,
-        address[] calldata _to,
-        uint256[] calldata _id
-    ) external {
-        if (_to.length == _id.length) revert();
-
-        for (uint256 i = 0; i < _to.length; i++) _token.transferFrom(msg.sender, _to[i], _id[i]);
-    }
-
     // _startTokenId from 1 not 0
     function _startTokenId() internal pure override returns (uint256) {
         return 1;
@@ -307,58 +294,3 @@ contract PresaleNft is NftComics {
         presaleActiveTime = _presaleActiveTime;
     }
 }
-
-/*
-    using Address for address;
-    using Strings for uint256;
-
-    // Compiler will pack this into a single 256bit word.
-    struct TokenOwnership {
-        // The address of the owner.
-        address addr;
-        // Keeps track of the start time of ownership with minimal overhead for tokenomics.
-        uint64 startTimestamp;
-        // Whether the token has been burned.
-        bool burned;
-    }
-
-    // Compiler will pack this into a single 256bit word.
-    struct AddressData {
-        // Realistically, 2**64-1 is more than enough.
-        uint64 balance;
-        // Keeps track of mint count with minimal overhead for tokenomics.
-        uint64 numberMinted;
-        // Keeps track of burn count with minimal overhead for tokenomics.
-        uint64 numberBurned;
-        // For miscellaneous variable(s) pertaining to the address
-        // (e.g. number of whitelist mint slots used).
-        // If there are multiple variables, please pack them into a uint64.
-        uint64 aux;
-    }
-
-    // The tokenId of the next token to be minted.
-    uint256 internal _currentIndex;
-
-    // The number of tokens burned.
-    uint256 internal _burnCounter;
-
-    // Token name
-    string private _name;
-
-    // Token symbol
-    string private _symbol;
-
-    // Mapping from token ID to ownership details
-    // An empty struct value does not necessarily mean the token is unowned. See ownershipOf implementation for details.
-    mapping(uint256 => TokenOwnership) internal _ownerships;
-
-    // Mapping owner address to address data
-    mapping(address => AddressData) private _addressData;
-
-    // Mapping from token ID to approved address
-    mapping(uint256 => address) private _tokenApprovals;
-
-    // Mapping from owner to operator approvals
-    mapping(address => mapping(address => bool)) private _operatorApprovals;
-
-*/
