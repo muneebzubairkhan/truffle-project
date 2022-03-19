a6d98481e101000aec7d3ccac8d4bc6499f11d31
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-20 00:04:32 +0500

    Visitors 0xB757419b9FB0F5964EDD957f5C6A8897C222a2Ab, VisitorsAddress 0x02F5991047f00f55A75043E28DD6ABf118399457

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,6 +2,6 @@ const __1 = artifacts.require("Visitors");
 const __2 = artifacts.require("VisitorsAddress");
 
 module.exports = async (deployer, network, accounts) => {
-  let _1 = await deployer.deploy(__1);
-  let _2 = await deployer.deploy(__2);
+  let _1 = await deployer.deploy(__1, { from: accounts[2] });
+  let _2 = await deployer.deploy(__2, { from: accounts[2] });
 };
