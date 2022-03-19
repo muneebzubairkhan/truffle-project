896a5353f44b274a23a95fec85afdcf2cc67cfd9
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-19 14:38:31 +0500

    VisitorCameAddress

--- contracts/Visitors.sol
+++ contracts/Visitors.sol
@@ -3,13 +3,30 @@
 pragma solidity ^0.8.0;
 
 contract Visitors {
-    event VisitorCame(uint256 timestamp);
-    uint256 public visitors;
+
+    // Owner
+
     address owner = msg.sender;
 
-    function visitorCame() external {
-        require(owner == msg.sender, "only owner");
+    modifier onlyOwner { require(owner == msg.sender, "only owner"); _;}
+
+    // VisitorCame
+
+    event VisitorCame(uint256 timestamp);
+    uint256 public visitors;
+
+    function visitorCame() external onlyOwner {
         visitors++;
         emit VisitorCame(block.timestamp);
+    }
+
+    // VisitorCameAddress
+
+    event VisitorCameAddress(uint256 timestamp, address visitor);
+    uint256 public visitorsWithAddress;
+
+    function visitorCameAddress(address _visitor) external onlyOwner {
+        visitorsWithAddress++;
+        emit VisitorCameAddress(block.timestamp, _visitor);
     }
 }
