ccc1a96fa3a3ddb4f6068b3dec50f480d6c199ee
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-19 23:53:09 +0500

    Already Verified: https://mumbai.polygonscan.com/address/0x9a8f4b1bbcF993942a6b0F11215092C963Ebb98a#code Verifying VisitorsAddress Pass - Verified: https://mumbai.polygonscan.com/address/0x612fB0bB1F65202bd11A352AE751Ef777C2d9daA#code

--- contracts/VisitorsAddress.sol
+++ contracts/VisitorsAddress.sol
@@ -8,14 +8,16 @@ contract VisitorsAddress {
     event VisitorCame(uint256 timestamp, address visitor);
     uint256 public visitors;
 
+    uint256 public uniqueVisitors;
+    mapping(address => bool) visited;
+
     function visitorCame(address _visitor) external {
         require(owner == msg.sender, "only owner");
         visitors++;
+        if (!visited[_visitor]) {
+            uniqueVisitors++;
+            visited[_visitor] = true;
+        }
         emit VisitorCame(block.timestamp, _visitor);
-    }
-
-    function visitorsReset() external {
-        require(owner == msg.sender, "only owner");
-        visitors = 0;
     }
 }
