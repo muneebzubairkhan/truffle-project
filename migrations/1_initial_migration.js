ca8e4ee0be37b0d0864ddc9214d45a42fa0ee0f3
Muneeb <muneeb.zubair.hash@gmail.com>
2022-03-27 19:37:40 +0500

    testing step by step

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -8,10 +8,13 @@ module.exports = async (deployer, network, [owner, account2, account3]) => {
 
   // if (network !== "development") return;
 
-  console.log(fromWei(await web3.eth.getBalance(owner)));
-  //
-  let nft1 = await deployer.deploy(Nft1, { from: owner });
-  let nft2 = await deployer.deploy(Nft2, { from: owner });
+  // console.log(fromWei(await web3.eth.getBalance(owner)));
+  // //
+  // // let nft1 = await deployer.deploy(Nft1, { from: owner });
+  // // let nft2 = await deployer.deploy(Nft2, { from: owner });
+  
+  // //
+  // let nft1 = await Nft1.deployed();
   // await nft1.setSaleActiveTime(0, { from: owner });
   // await nft1.purchaseTokens(2, {
   //   value: toWei("0.00120", "ether"),
@@ -21,8 +24,8 @@ module.exports = async (deployer, network, [owner, account2, account3]) => {
   // await nft1.withdraw({ from: owner });
   // console.log(fromWei(await web3.eth.getBalance(owner)));
 
-  // //
-  // let nft2 = await deployer.deploy(Nft2, { from: owner });
+  //
+  let nft2 = await Nft2.deployed();
   // await nft2.setGoldenTicket(nft1.address, { from: owner });
   // await nft2.setSaleActiveTime(0, { from: owner });
   // await nft2.purchaseTokens(2, {
@@ -33,11 +36,11 @@ module.exports = async (deployer, network, [owner, account2, account3]) => {
   // await nft2.withdraw({ from: owner });
   // console.log(fromWei(await web3.eth.getBalance(owner)));
 
-  // await nft2.purchaseTokensWithGoldenTickets([1, 2], {
-  //   from: owner,
-  //   value: toWei("0.00120", "ether"),
-  // });
-  // console.log(fromWei(await web3.eth.getBalance(owner)));
-  // await nft2.withdraw({ from: owner });
-  // console.log(fromWei(await web3.eth.getBalance(owner)));
+  await nft2.purchaseTokensWithGoldenTickets([1, 2], {
+    from: owner,
+    value: toWei("0.00120", "ether"),
+  });
+  console.log(fromWei(await web3.eth.getBalance(owner)));
+  await nft2.withdraw({ from: owner });
+  console.log(fromWei(await web3.eth.getBalance(owner)));
 };;
