6eab58e8c3f185f1e4d01f7a153ef8c15d923c1b
Muneeb <muneeb.zubair.hash@gmail.com>
2022-05-31 17:13:05 +0500

    ERC20Token

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -16,21 +16,9 @@ contract ERC20Token is Ownable, Pausable, ERC20("ERC20Token", "ETK"), ERC20Burna
     // mint limit
     uint mintingLimit = 1 * 1e9 * 1e18; // 1 Billion Supply = 1 * 9 zeros * 18 zeros = (1 * 9 zeros = 1 Billion) * (18 zeros = 18 decimals)
 
-    function setMintingLimit(uint _mintingLimit) external onlyOwner {
-        mintingLimit = _mintingLimit;
-    }
-
-    // minting
-    bool public mintStopped = false;
-
     function mint(address account, uint256 amount) public onlyOwner {
-        require(!mintStopped, "mint is stopped");
         _mint(account, amount);
         require(totalSupply() <= mintingLimit, "Increase the mint limit first.");   
-    }
-
-    function stopMint() public onlyOwner {
-        mintStopped = true;
     }
 
     // white list
@@ -41,26 +29,8 @@ contract ERC20Token is Ownable, Pausable, ERC20("ERC20Token", "ETK"), ERC20Burna
     }
 
     function whitelist_mint(address account, uint256 amount) external whenNotPaused {
-        require(!mintStopped, "mint is stopped");
         require(whitelist[msg.sender], "Sender must be whitelisted");
         _mint(account, amount);
         require(totalSupply() <= mintingLimit, "Ask the owner to increase the mint limit first.");   
-    }
-
-    // ERC20Pausable
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override whenNotPaused {
-        super._beforeTokenTransfer(from, to, amount);
     }
 }
