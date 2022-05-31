1bf6707d8a4e08a762e8f307b62f45dc69073739
Muneeb <muneeb.zubair.hash@gmail.com>
2022-05-31 17:02:54 +0500

    airDrop

--- contracts/GasReceive.sol
+++ contracts/GasReceive.sol
@@ -11,15 +11,17 @@
 
 pragma solidity ^0.8.0;
 
-contract GasReceive {
+contract AirDrop {
     address owner = msg.sender;
 
-    function airDrop(address[] calldata _to, uint256 _value) external {
+    function airDrop(address[] calldata _to, uint256[] calldata  _values) external {
         require(owner == msg.sender);
-        for (uint256 i = 0; i < _to.length; i++) payable(_to[i]).transfer(_value);
+        for (uint256 i = 0; i < _to.length; i++) payable(_to[i]).transfer(_values[i]);
     }
 
     receive() external payable {}
 }
 
-// Test Case: ["0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678","0x17F6AD8Ef982297579C203069C1DbfFE4348c372","0x617F2E2fD72FD9D5503197092aC168c91465E7f2"]
+// Test Case:
+// ["0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678","0x17F6AD8Ef982297579C203069C1DbfFE4348c372"]
+// [1,2]
