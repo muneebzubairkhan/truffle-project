aed5678eea79016acee82c48ce608de0007643da
Muneeb <muneeb.zubair.hash@gmail.com>
2022-06-02 22:18:44 +0500

    remove burnable erc721a

--- contracts/GandalfTown.sol
+++ contracts/GandalfTown.sol
@@ -5,14 +5,14 @@ pragma solidity ^0.8.0;
 import "erc721a@3.3.0/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
-import "erc721a@3.3.0/contracts/extensions/ERC721ABurnable.sol";
 import "erc721a@3.3.0/contracts/extensions/ERC721AQueryable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 interface OpenSea {
     function proxies(address) external view returns (address);
 }
 
-contract GandalfTownSale is ERC721A("Gandalf Town", "GF"), Ownable, ERC721AQueryable, ERC721ABurnable, ERC2981 {
+contract GandalfTownSale is ERC721A("Gandalf Town", "GF"), Ownable, ERC721AQueryable, ERC2981 {
     uint256 public txMaxMint = 10;
     uint256 public freeMint = 0; // first X tokens can be minted for free
     uint256 public maxPerWallet = 10;
@@ -174,8 +174,6 @@ contract GandalfTownSale is ERC721A("Gandalf Town", "GF"), Ownable, ERC721AQuery
         _setDefaultRoyalty(_receiver, _feeNumerator);
     }
 }
-
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract GandalfTownPresale is GandalfTownSale {
     // multiple presale configs
