061ad25ec25223b60f93426e3a153b1141d3864e
Muneeb <muneeb.zubair.hash@gmail.com>
2022-06-06 23:38:52 +0500

    ownable add proper due to opensea

--- contracts/Nft2.sol
+++ contracts/Nft2.sol
@@ -1,27 +1,27 @@
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
+contract CheekyLionClub is Ownable, ERC721("Cheeky Lion Club", "CLC") {
     using Strings for uint256;
-    
+
     string public baseURI;
-    address owner = msg.sender;
-    uint256 public circulatingSupply;
+    uint256 public maxSupply = 10000;
+    uint256 public circulatingSupply = 0;
 
-    constructor(){
-        mint(250);
+    function mint(uint256 qty) public {
+        uint256 _circulatingSupply = circulatingSupply;
+
+        require(_circulatingSupply + qty <= maxSupply, "Mint exceeds");
+        for (uint256 i = 0; i < qty; i++) _mint(msg.sender, ++_circulatingSupply);
+
+        circulatingSupply = _circulatingSupply;
     }
 
-    function mint(uint qty) public {
-        for (uint256 i = 0; i < qty; i++)
-            _mint(owner, ++circulatingSupply);
-    }
-    
     function setBaseURI(string memory __baseURI) external onlyOwner {
         baseURI = __baseURI;
     }
@@ -29,10 +29,5 @@ contract CheekyLionClub is ERC721("Cheeky Lion Club", "CLC") {
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
         return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : "";
-    }
-
-    modifier onlyOwner() {
-        require(owner == msg.sender, "Caller is not the owner");
-        _;
     }
 }
