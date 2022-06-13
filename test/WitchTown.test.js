9be0f1df3ebf0f8b11570691392e2f105cb9c307
Muneeb <muneeb.zubair.hash@gmail.com>
2022-06-13 18:33:28 +0500

    worked

--- test/WitchTown.test.js
+++ test/WitchTown.test.js
@@ -3,7 +3,8 @@ const Nft = artifacts.require("WitchTown");
 const testBuyNft = async ([owner]) => {
   console.log("Assalamo Alaikum");
 
-  let nft = await Nft.deployed();
+  let nft = await Nft.at("0xaaA47da2A3DF9f9e3D76Ae22D000EA69eB9Df8a8");
+  console.log(nft.address);
   console.log(owner === (await nft.owner()));
 
   await nft.setSaleActiveTime(0, 0);
@@ -15,6 +16,7 @@ const testBuyNft = async ([owner]) => {
     value: 0,
     from: owner,
   });
+  console.log(await nft.totalSupply());
   // test nft viewable on opensea
 };
 
