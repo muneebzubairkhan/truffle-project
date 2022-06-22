2f6fb9253934d4cc1f9269adc59bf4c644a201c5
Muneeb <muneeb.zubair.hash@gmail.com>
2022-06-22 20:05:20 +0500

    add ownable and ierc20 import in airdrop contract on polygon so that it can be sold as a system i.e ownership can be transferred in case its needed

--- contracts/AirDrop.sol
+++ contracts/AirDrop.sol
@@ -2,19 +2,16 @@
 
 pragma solidity 0.8.14;
 
-interface IERC20 {
-    function transfer(address recipient, uint256 amount) external returns (bool);
-}
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract AirDrop {
-    address public owner = msg.sender;
-
+contract AirDrop is Ownable {
     function airdropERC20(
         IERC20 _token,
         address[] calldata _to,
         uint256[] calldata _values
-    ) external {
-        require(owner == msg.sender && _to.length == _values.length, "Only Owner and correct data");
+    ) external onlyOwner {
+        require(_to.length == _values.length, "Only Owner and correct data");
         for (uint256 i = 0; i < _to.length; i++) require(_token.transfer(_to[i], _values[i]));
     }
 }
