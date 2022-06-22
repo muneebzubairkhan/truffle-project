c845005c35bb7fe43e55881b6c67c28f5d37839e
Muneeb <muneeb.zubair.hash@gmail.com>
2022-06-22 19:34:05 +0500

    contract shortened

--- contracts/AirDrop.sol
+++ contracts/AirDrop.sol
@@ -12,24 +12,12 @@ interface IERC20 {
 
 contract AirDrop {
     address public owner = msg.sender;
-
     function airdropERC20(
         IERC20 _token,
         address[] calldata _to,
         uint256[] calldata _values
-    ) public {
-        require(owner == msg.sender);
-        require(_to.length == _values.length, "Receivers and amounts are different length");
+    ) external {
+        require(owner == msg.sender && _to.length == _values.length, "Only Owner and correct data");
         for (uint256 i = 0; i < _to.length; i++) require(_token.transferFrom(address(this), _to[i], _values[i]));
     }
-
-    function airDrop(address[] calldata _to, uint256[] calldata _values) external {
-        require(owner == msg.sender);
-        require(_to.length == _values.length, "Receivers and amounts are different length");
-        for (uint256 i = 0; i < _to.length; i++) payable(_to[i]).transfer(_values[i]);
-    }
-
-    function getToken() external payable {}
-
-    receive() external payable {}
 }
