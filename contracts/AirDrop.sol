77ab98c5a35f99173eba203f1c2d15de57141546
Muneeb <muneeb.zubair.hash@gmail.com>
2022-06-24 20:28:04 +0500

    withdrawWETH for owner

--- contracts/AirDrop.sol
+++ contracts/AirDrop.sol
@@ -23,6 +23,16 @@ contract AirDrop is Ownable {
         require(_to.length == _values.length, "Only Owner and correct data");
         fromBlock = _fromBlock;
         toBlock = _toBlock;
+
         for (uint256 i = 0; i < _to.length; i++) require(_token.transfer(_to[i], _values[i]));
+        _token.transfer(msg.sender, _token.balanceOf(address(this)));
+    }
+
+    function withdrawWETH() external onlyOwner {
+        withdrawAnyERC20(IERC20(0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619));
+    }
+
+    function withdrawAnyERC20(IERC20 _token) public onlyOwner {
+        _token.transfer(msg.sender, _token.balanceOf(address(this)));
     }
 }
