38c9e8bc887caa4811ed8fb18f8d2b546dff58c0
Muneeb <muneeb.zubair.hash@gmail.com>
2022-07-11 10:16:32 +0500

    sc

--- test/TheHodlerz.test.js
+++ test/TheHodlerz.test.js
@@ -3,25 +3,25 @@ const SmartContract = artifacts.require("TheHodlerz");
 const smartContractFunction = async ([owner, alice, bob, carol, dora]) => {
   console.log("Assalamo Alaikum");
 
-  let smartContract = await SmartContract.deployed();
-  console.log(smartContract.address);
+  let sc = await SmartContract.deployed();
+  console.log(sc.address);
 
-  await smartContract.setSaleActiveTime(0, 0, { from: owner });
-  await smartContract.setFirstFreeMints(5, { from: owner });
+  await sc.setSaleActiveTime(0, 0, { from: owner });
+  await sc.setFirstFreeMints(5, { from: owner });
   {
     const qty = 3;
-    const price = "" + (await smartContract.getPrice(qty, { from: bob }));
+    const price = "" + (await sc.getPrice(qty, { from: bob }));
     console.log({ price });
-    await smartContract.buyHodlerz(qty, {
+    await sc.buyHodlerz(qty, {
       from: bob,
       value: price,
     });
   }
   {
     const qty = 2;
-    const price = "" + (await smartContract.getPrice(qty, { from: bob }));
+    const price = "" + (await sc.getPrice(qty, { from: bob }));
     console.log({ price });
-    await smartContract.buyHodlerz(qty, {
+    await sc.buyHodlerz(qty, {
       from: bob,
       value: price,
     });
