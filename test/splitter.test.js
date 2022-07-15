349ed7df95959578f56825b5267c1cf740017d1a
Muneeb <muneeb.zubair.hash@gmail.com>
2022-07-15 20:28:17 +0500

    tested weth and erc20 withdraw

--- test/splitter.test.js
+++ test/splitter.test.js
@@ -1,7 +1,8 @@
 const Splitter = artifacts.require("RoyaltySplit");
+const WETH9 = artifacts.require("WETH9");
 
 contract("Nft", async ([owner, client, parentCompany]) => {
-  it("deploy smart contract", async () => {
+  it("withdraw ETH", async () => {
     let splitter = await Splitter.new({ from: owner });
 
     await web3.eth.sendTransaction({
@@ -10,5 +11,22 @@ contract("Nft", async ([owner, client, parentCompany]) => {
       value: "100",
       gas: 25000,
     });
+
+    splitter.withdraw();
+  });
+
+  it("withdraw ERC20", async () => {
+    let splitter = await Splitter.new({ from: owner });
+    let weth9 = await WETH9.new({ from: owner });
+
+    await web3.eth.sendTransaction({
+      from: owner,
+      to: weth9.address,
+      value: "100",
+    //   gas: 25000,
+    });
+    await weth9.transfer(splitter.address, "100");
+
+    splitter.withdrawERC20();
   });
 });
